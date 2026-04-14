0128363b3fcca296eb61b224b4691df22c1c7959
sdemattia <sdematti@uci.edu>
2026-04-14 14:02:39 -0700

    start and map scenes + transitions
    start and map scene
    start and level one button
    transitions between all scenes
    start -> map -> game

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Buttons : MonoBehaviour 
+{
+    [SerializeField] private SceneController _sceneController; 
+    
+    public void StartButton() 
+    { 
+        _sceneController.LoadMapScene(); 
+    } 
+    
+    public void LevelOneButton() 
+    {
+         _sceneController.LoadGameScene(); 
+    } 
+}
