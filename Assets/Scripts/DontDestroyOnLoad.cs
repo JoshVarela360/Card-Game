f60f50df9c660a48d88f04a0bf5cbea788644597
Joshua Varela <jnvarel1@uci.edu>
2026-05-06 00:08:54 -0700

    Fixed all in game audio

--- Assets/Scripts/DontDestroyOnLoad.cs
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    public static DontDestroyOnLoad _audioManagerInstance {get; private set;}
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       DontDestroyOnLoad(gameObject); 
+       
+            DontDestroyOnLoad(gameObject);
+        
+         
+       
     }
+    
 
    
 }
